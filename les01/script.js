9c8de8431139fd483dd1ef0e7ff8afe1dea2d60d
Alex Khayev <alexander.khayev@gmail.com>
2020-03-20 18:28:29 +0400

    добавил своё видение по п.2 задачки из методички.

--- les01/script.js
+++ les01/script.js
@@ -5,15 +5,21 @@ const goods = [
 { title: 'Shoes', price: 250},
 ];
 
-const  renderGoodsItem = (title, price) => {
+const  renderGoodsItem = ({title, price} = {}) => {
   return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 };
 
 const renderGoodsList = (list) => {
-  // запятая между элементами корзины из-за того, что map создает новый массив, а
+  // п.3 методички: запятая между элементами корзины из-за того, что map создает новый массив, а
   // toString склеивает элементы массива в строку через запятую. Поэтому добавим join(""),
   // чтобы убрать ненужные нам запятые в верстке
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join("");
+
+  // п.2 методички - добавил значения по умолчанию и типа немного упростил. Теперь независимо от того,
+  // как будет расширяться функция renderGoodItem, в вызывающем ее коде не надо будет дописывать везде
+  // новые параметры. Все что есть в передаваемом объекте она возьмет сама за счет деструктуризации.
+  // Остается вопрос, как передать туда сразу текущий item, не указывая его (навевает сюда bind, но
+  // не соображу пока как прикрутить)
+  let goodsList = list.map(item => renderGoodsItem(item)).join("");
   document.querySelector('.goods-list').innerHTML = goodsList;
 };
 
