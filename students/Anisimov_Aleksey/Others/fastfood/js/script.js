7e630a2bc00698baa39420abb448566c78f66144
1XTR.homepc <1XTR.homepc@trendco.space>
2020-03-24 01:04:26 +0300

    fastfood begin

--- students/Anisimov_Aleksey/Others/fastfood/js/script.js
+++ students/Anisimov_Aleksey/Others/fastfood/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+let allInputs = document.querySelectorAll('input');
+
+allInputs.forEach(item => item.addEventListener('change', actionOnChange));
+
+
+function actionOnChange() {
+    console.log(event);
+}
