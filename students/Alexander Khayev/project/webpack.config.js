a11f53d7905f8bde6c0de9e8ef062d34d9ecb73a
Alex Khayev <alexander.khayev@gmail.com>
2020-03-30 20:50:05 +0400

    добавил в webpack складирование json файлов в dist

--- students/Alexander Khayev/project/webpack.config.js
+++ students/Alexander Khayev/project/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const miniCss = require('mini-css-extract-plugin');
 const htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // context: path.join(__dirname, 'your-app'),
   plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/static',
+      to: 'static'},
+      { from: './src/**/*.json',
+      to: 'api/[name].[ext]'}
+    ]),
     new miniCss({
         filename: 'css/[name].css',
         chunkFilename: '[id].css',
@@ -14,7 +22,7 @@ module.exports = {
       {
         template: './src/index.html',
       }
-    )
+    ),
   ],
   module: {
     rules: [
@@ -28,7 +36,12 @@ module.exports = {
             esModule: true
           }
         }, 'css-loader'],
-      }
-    ]
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    port: 3000
   }
 }
