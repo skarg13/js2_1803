c27142045c8782d2c9d9b9907dda4cc90eb0313f
Victor <gystys@gmail.com>
2020-03-31 23:27:05 +0700

    Lessons 4 task 4.1 and task 4.2 ready

--- students/Victor_Iustus/additionals/task4/src/public/js/main.js
+++ students/Victor_Iustus/additionals/task4/src/public/js/main.js
@@ -1,5 +1,21 @@
+//task1
+function regexpTask1(text) {
+    return text.replace( /\'/g, '\"');
+}
 
+//task2
+function regexpTask2(text) {
+    const regexp  = /(?<=\S)\'(?!\S)|(?<!\S)\'(?=\S)/g;
+    return text.replace(regexp, '\"');
+}
 
 export default function app() {
 
+    //task1
+    var text = document.querySelector("#task1").innerHTML
+    document.querySelector("#task1-result").innerHTML = regexpTask1(text)
+
+    //task2
+    var text = document.querySelector("#task2").innerHTML
+    document.querySelector("#task2-result").innerHTML = regexpTask2(text)
 }
