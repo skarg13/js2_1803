f842e0192f494f986d969cc4fd51c8c355124cdf
andykey-krsk <andykey@bk.ru>
2020-04-03 23:16:07 +0700

    Доделал метода корзины добавить, удалить, посчитать сумму

--- students/Andrey Kudryavtsev/Project/src/public/js/main.js
+++ students/Andrey Kudryavtsev/Project/src/public/js/main.js
@@ -1,4 +1,4 @@
-let API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'; // /getBasket.json
+let API = 'https://raw.githubusercontent.com/andykey-krsk/online-store-api/master/responses'; 
 
 class List {
     //catalog and cart
@@ -7,42 +7,29 @@ class List {
         this.url = url
         this.container = container
         this.items = []
-        //console.log(this.constructor.name)
         this._init()
-        //this.getData(url)
-
     }
     _init() {
         return false
     }
     getData(url) {
         return fetch(API + url).then(dataReceived => dataReceived.json())
-        // return new Promise ((res, rej) => {
-        //     let xhr = new XMLHttpRequest();
-
-        //     xhr.onreadystatechange = function () {
-        //         if (xhr.readyState === 4) {
-        //             console.log('hello oop')
-        //             res(xhr.responseText);
-        //         } else {
-        //             rej(err)
-        //         }
-        //     }
-        //     xhr.open('GET', url, true);
-        //     xhr.send();
-        // })
     }
     render() {
         let block = document.querySelector(this.container)
         let htmlString = ''
         this.items.forEach(item => {
-            // debugger
             htmlString += new lists[this.constructor.name](item).render()
         })
         block.innerHTML = htmlString
+
+        if (this.constructor.name == 'Cart'){
+            this._checkTotalAndSum ()
+            this.quantityBlock.innerText = this.total
+            this.priceBlock.innerText = this.sum
+        }
     }
 }
-
 
 class ListItem {
     constructor(item) {
@@ -99,6 +86,10 @@ class Catalog extends List {
 class Cart extends List {
     constructor(url, container) {
         super(url = '/getBasket.json', container = '.cart-items')
+        this.total = 0
+        this.sum = 0
+        this.quantityBlock = document.querySelector ('#quantity')
+        this.priceBlock = document.querySelector ('#price')
     }
     _init() {
         this.getData(this.url)
@@ -121,8 +112,50 @@ class Cart extends List {
     }
     addProduct(item) {
         console.log('Bought: ' + item.dataset.name)
-    }
 
+        let id = item.dataset['id']
+        let find = this.items.find (product => product.id_product === id)
+        if (find) {
+            find.quantity++
+        } else {
+            let prod = this._createNewProduct (item)
+            this.items.push (prod)
+        }
+        this._checkTotalAndSum ()
+        this.quantityBlock.innerText = this.total
+        this.priceBlock.innerText = this.sum
+        this.render()
+    }
+    _createNewProduct (prod) {
+        return {
+            product_name: prod.dataset['name'],
+            price: prod.dataset['price'],
+            id_product: prod.dataset['id'],
+            quantity: 1
+        }
+    }
+    deleteProduct(item) {
+        console.log('Del: ' + item.dataset.id)
+        let id = item.dataset['id']
+        let find = this.items.find (product => product.id_product === id)
+        if (find.quantity > 1) {
+            find.quantity--
+        } else {
+            this.items.splice (this.items.indexOf(find), 1)
+        }
+        this._checkTotalAndSum ()
+        this.render ()
+    }
+    _checkTotalAndSum () {
+        let qua = 0
+        let pr = 0
+        this.items.forEach (item => {
+            qua += item.quantity
+            pr += item.price * item.quantity
+        })
+        this.total = qua
+        this.sum = pr
+    }
 }
 
 class CatalogItem extends ListItem {
@@ -161,5 +194,5 @@ export default function () {
     let testCat = new Catalog()
     let testCrt = new Cart()
 
-    testCat.cart = testCrt
+   // testCat.cart = testCrt
 }
