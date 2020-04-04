7851be36e78678d4d4ea7b91d48eb6a9a20a30a4
1XTR.homepc <1XTR.homepc@trendco.space>
2020-04-04 17:31:51 +0300

    vue components

---
+++ students/Anisimov_Aleksey/Project/src/components/item.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <template v-if="type === 'catalog'">
+
+        </template>
+        <template v-if="type === 'cart'">
+            // шаблон html для корзины
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'catalog'
+            //default: () => 'catalog'
+        },
+        item: {
+            type: Object
+        }
+    },
+    computed: {
+        imgComputed
+    }
+}
+</script>
---
+++ students/Anisimov_Aleksey/Project/src/containers/catalog.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="product center">
+        <item />
+
+    </div>
+</template>
+
+<script>
+import item from '../components/item.vue'
+export default {
+    components: {item},
+    data() {
+        return {
+            items: {},
+            url: 'https://static.trendco.space/js-adv/responses/goods.json'
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    mounted() {
+        this.$parent.getData(url)
+            .then(data => this.items = data)
+    }
+}
+</script>
