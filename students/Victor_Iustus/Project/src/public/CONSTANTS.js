787e4db7afa3e5f1635b932fcfe6c9a1471d97a4
Victor <gystys@gmail.com>
2020-04-06 05:50:16 +0700

    Lessons 5,6 finish

---
+++ students/Victor_Iustus/Project/src/public/CONSTANTS.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://raw.githubusercontent.com/Indaled/online-store-api/master/responses'
+
+export const IMG_PLACEHOLDER        = 'https://placehold.it/300x200'
+export const IMG_PLACEHOLDER_CART   = 'https://placehold.it/100x80'
+
+export const URL_GET_CATALOG    = '/catalogData.json'
+export const URL_GET_CART       = '/getBasket.json'
+export const URL_ADD_CART       = '/addToBasket.json'
+export const URL_DEL_CART       = '/deleteFromBasket.json'
