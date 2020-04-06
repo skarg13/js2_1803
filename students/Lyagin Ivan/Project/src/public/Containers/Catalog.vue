3516911403e357c05c5eec97581f88eb486e58ff
Niga9L <lyaginvanya@gmail.com>
2020-04-07 01:01:18 +0300

    Vue 6 HW

--- students/Lyagin Ivan/Project/src/public/Containers/Catalog.vue
+++ students/Lyagin Ivan/Project/src/public/Containers/Catalog.vue
@@ -1,10 +1,28 @@
 <template>
-    $END$
+    <div>
+        <div class="products">
+            <item v-for="item of items" :key="item.id_product" :item="item"/>
+        </div>
+    </div>
 </template>
 
 <script>
+    import item from '../Components/Item.vue'
     export default {
-        name: "Catalog"
+        components: { item },
+        data() {
+            return {
+                items: [],
+                url: 'https://raw.githubusercontent.com/kellolo/js2_1803/master/students/Alexander%20Khayev/project/src/api/catalogData.json',
+            }
+        },
+        mounted() {
+            this.$parent.getData(this.url)
+                .then(data => {
+                    this.items = data;
+                });
+            console.log(this.items);
+        }
     }
 </script>
 
--- students/Lyagin Ivan/Project/src/public/index.vue
+++ students/Lyagin Ivan/Project/src/public/index.vue
@@ -1,13 +1,29 @@
 <template>
-    $END$
+    <div>
+        <header>
+            <div class="logo">E-shop</div>
+            <cart ref="cartRef"/>
+        </header>
+        <main>
+            <catalog ref="catalogRef"/>
+        </main>
+    </div>
 </template>
 
 <script>
+    import catalog from './Containers/Catalog.vue'
+    import cart from './Containers/Cart.vue'
     export default {
-        name: "index"
+        components: { catalog, cart },
+        methods: {
+
+            getData (url) {
+                return fetch(url).then(dataReceived => dataReceived.json())
+            },
+        }
     }
 </script>
 
-<style scoped>
+<style>
 
 </style>
