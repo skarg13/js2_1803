c54184a71441d301e0cf62325cc72243c8425c1c
NyashMyash491 <59206582+NyashMyash491@users.noreply.github.com>
2020-04-08 04:57:13 +0500

    123

---
+++ students/Girfanov Artur/PROJECT/6_vue_components/Item.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="type === 'catalog' ? 'product-item' : 'cart-item'">
+        <template v-if="type === 'catalog'">
+            <img :src="imgCompute" :alt="item.product_name">
+            <div class="desc">
+                <h1>{{ item.product_name }}</h1>
+                <p>{{ item.price }}</p>
+                <button class="buy-btn">Купить</button>
+            </div>
+        </template>
+
+        <template v-if="type === 'cart'">
+            <img :src="imgCompute" :alt="item.product_name">
+            <div class="product-detail">
+                <!-- {{item.product_name}}
+                {{item.quantity}}
+                <{{item.price}}-->
+            </div>
+
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        //props: ['type']
+        props: {
+            type: {
+                type: String,
+                default: 'catalog'
+                //default: () => 'catalog'
+            },
+            item: {
+                type: Object
+            }
+        },
+        computed: {
+            imgCompute() {
+                return `https://placehold.it/${this.type === 'catalog' ? '300x200' : '100x80'}`
+            }
+        }
+    }
+</script>
---
+++ students/Girfanov Artur/PROJECT/6_vue_components/app.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header>
+            <div class="logo">E-shop</div>
+            <search/>
+            <div class="cart" id="cart">
+                <button class="btn-cart" @click="$refs.cart.show = !$refs.cart.show">Cart</button>
+                <cart ref="cart"/>
+            </div>
+        </header>
+        <main >
+            <catalog ref="catalog"/>
+        </main>
+    </div>
+</template>
+
+<script>
+import catalog from './containers/Catalog.vue'
+import cart from './containers/Cart.vue'
+export default {
+    components: {catalog, cart},
+    methods: {
+        getData(url) {  
+            return  fetch(url).then(data => data.json())
+        },
+    }
+}
+</script>
