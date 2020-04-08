155f7e845b945ad9425e583eb65b9fb523029f55
Dmitriy Koreshkov <kore.dmi@gmail.com>
2020-04-08 14:49:21 +0300

    Домашняя работа 6

---
+++ students/Dmitriy Koreshkov/others/Shop/src/public/components/item.vue
@@ -0,0 +1,49 @@
+ <template>
+    <div :class="type === 'catalog' ? 'product-item' : 'cart-item'">
+        <template v-if="type === 'catalog'">
+            <img :src="imgCompute" :alt="item.product_name">
+            <div class="desc">
+                <h1>{{item.product_name}}</h1>
+                <p>{{item.price}}</p>
+                <button class="buy-btn" >Купить</button>
+            </div>   
+        </template>
+
+        <template v-if="type === 'cart'">
+        <div :class="type === 'cart' ? 'cart-item' : 'product-item'">
+            <img :src="imgCompute" :alt="item.product_name">
+            <div class="product-desc">
+                <p class="product-title">{{item.product_name}}</p>
+                <p class="product-quantity">{{item.quantity}}</p>
+                <p class="product-single-price">{{item.price}}</p>
+            </div>
+            <div class="right-block">
+            <button class="del-btn">&times;</button>
+            </div>
+        </div>
+        
+        </template>
+    </div>    
+
+</template>
+
+<script>
+export default {
+    //props: ["type"]
+    props: {
+        type:{
+            type: String,
+            default: "catalog"
+            //default: () => "catlog"
+        },
+        item: {
+            type: Object 
+        }
+    },
+    computed: {
+        imgCompute(){
+            return `https://placehold.it/${this.type === 'catalog' ? '300x200' : '100x80'}`
+        }
+    },
+}
+</script>
