bea5725c4cfd5649ce5dbe62fb6f44054492b69f
Egorarefjev <38657241+Egorarefjev@users.noreply.github.com>
2020-04-08 14:03:52 +0300

    Create app.vue

---
+++ students/Arefjev Egor/Project/src/publick/app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <header>
+            <div class="logo">E-shop</div>
+            <cart ref="cartRef"/>
+        </header>
+        <main>
+            <catalog ref="catalogRef"/>
+        </main>
+    </div>
+</template>
+
+<script>
+    import catalog from './Containers/Catalog.vue'
+    import cart from './Containers/Cart.vue'
+    export default {
+        components: { catalog, cart },
+        methods: {
+            getData (url) {
+                return fetch(url).then(dataReceived => dataReceived.json())
+            },
+        }
+    }
+</script>
+
+<style>
+</style>
