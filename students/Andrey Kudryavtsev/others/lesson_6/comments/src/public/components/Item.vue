ab10666952873439c7f7013d0e0f645048dfe621
andykey-krsk <andykey@bk.ru>
2020-04-08 18:49:56 +0700

    commensts

---
+++ students/Andrey Kudryavtsev/others/lesson_6/comments/src/public/components/Item.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="user">
+            <strong>{{ item.name }}</strong>
+        </div>
+        <div class="post-body">
+            <p>{{ item.body }}</p>
+        </div>
+        <div class="post-footer">
+            <a href="#">{{ item.mail }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+// export default {
+//     props: {
+//         item: {
+//             type: Object
+//         }
+//     }
+// }
+</script>
---
+++ students/Andrey Kudryavtsev/others/lesson_6/comments/src/public/containers/comments.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <item class="post" v-for="item of items" :key="item.id" />
+    </div>
+</template>
+
+<script>
+import item from '../components/Item.vue'
+export default {
+    components: { item },
+    data() {
+        return {
+            url: 'https://jsonplaceholder.typicode.com/comments',
+            items: []
+        }
+    },
+    methods: {
+        getData(url) {
+            return fetch(url).then(dataReceived => dataReceived.json())
+            
+            // try {
+            //    this.items = await fetch(url).then(d => d.json()) 
+            // }
+            // catch(err) {
+            //     console.log(err)
+            // }
+            // finally {
+            //     console.log(this.items)
+            // }
+        }
+    },
+    mounted() {
+        this.getData(this.url).then(data => {
+            this.items = data
+        })
+    }
+}
+</script>
